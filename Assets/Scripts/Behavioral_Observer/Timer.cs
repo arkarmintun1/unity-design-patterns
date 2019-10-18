dfb4ea9cdb6793d378270ea242bf7893a7d6b08d
Arkar Min Tun <arkarmintun.360ed@gmail.com>
2019-10-18 18:02:26 +0630

    AKMT: Observer in progress

---
+++ Assets/Scripts/Behavioral_Observer/Timer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    private float m_Duration = 10.0f;
+    private float m_HalfTime;
+
+    public delegate void TimerStarted();
+    public static event TimerStarted OnTimerStarted;
+
+    public delegate void HalfTime();
+    public static event HalfTime OnHalfTime;
+
+    public delegate void TimerEnded();
+    public static event TimerEnded OnTimerEnded;
+
+    private IEnumerator m_Coroutine;
+
+    IEnumerator Start()
+    {
+        m_HalfTime = m_Duration / 2;
+        
+        if (OnTimerStarted != null)
+        {
+            OnTimerStarted();
+        }
+
+        yield return StartCoroutine(WaitAndPrint(1.0f));
+
+        if (OnTimerEnded != null)
+        {
+            OnTimerEnded();
+        }
+    }
+
+    private IEnumerator WaitAndPrint(float waitTime)
+    {
+        while (Time.time < m_Duration)
+        {
+            yield return new WaitForSeconds(waitTime);
+
+            Debug.Log("Seconds: " + Mathf.Round(Time.time));
+
+            if (Mathf.Round(Time.time) == Mathf.Round(m_HalfTime))
+            {
+                if (OnHalfTime != null)
+                {
+                    OnHalfTime();
+                }
+            }
+        }
+    }
+}
